d26e44b2063d488828eabf0121be4aabe31d10bc
Kiril Knysh <k.knysh@gmail.com>
2017-08-02 23:25:37 +0200

    [#23] Add QD Auto Login strategy;

---
+++ src/auth/qd-auto-strategy/strategy.js
@@ -0,0 +1,49 @@
+const { URL } = require('url');
+const util = require('util');
+
+const passport = require('passport');
+const uuidv4 = require('uuid/v4');
+
+const { generateName } = require('./names');
+
+function Strategy(options, verify) {
+    passport.Strategy.call(this);
+
+    this.name = 'qd-auto';
+    this._callbackURL = options.callbackURL;
+    this._verify = verify;
+}
+
+util.inherits(Strategy, passport.Strategy);
+
+Strategy.prototype.authenticate = function (req) {
+    if (!req.query || req.query.qdAutoDone !== 'done') {
+        const callbackURL = new URL(this._callbackURL);
+
+        callbackURL.searchParams.append('qdAutoDone', 'done');
+        return this.redirect(callbackURL.href);
+    }
+
+    const user = {
+        id: uuidv4(),
+        displayName: generateName(),
+    };
+    user[Symbol.for('qd-criteria')] = {
+        uid: req.cookies && req.cookies.secret,
+    };
+
+    this._verify(null, null, user, (err, user, info) => {
+        if (err) {
+            return this.error(err);
+        }
+        if (!user) {
+            return this.fail(info);
+        }
+
+        info = info || {};
+
+        this.success(user, info);
+    });
+};
+
+module.exports = Strategy;
