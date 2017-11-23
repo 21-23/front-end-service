13e0aa7f70e9bc3a143548eb2fe0e1c7b0d353e4
Kiril Knysh <k.knysh@gmail.com>
2017-11-23 18:50:55 +0100

    Use identity from ✉️ 🏭

---
+++ config/index.js
@@ -0,0 +1,24 @@
+const nconf = require('nconf');
+
+// api keys
+// mongodb connection config
+// port
+
+const config = nconf.argv().env();
+
+config.defaults({
+    MONGO_URI: 'mongodb://127.0.0.1/fe-db',
+    PORT: 3000,
+    WS_PORT: 3001,
+    ARNAUX_URL: 'ws://localhost:8888/',
+    AUTH_CONFIG: 'config.secret.json',
+    ENABLE_GUNSLINGER: true,
+    'session-secret': 'keyboard cat',
+    userCacheOptions: {
+        max: 1000,
+        maxAge: 120 * 60 * 1000, // 2 hours
+    },
+    LOG_LEVEL: 'all',
+});
+
+module.exports = config;
