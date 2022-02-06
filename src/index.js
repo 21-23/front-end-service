6e76e4c297cb1066cdf7a957f3ddb2a7b5a50d5d
Kiril Knysh <k.knysh@gmail.com>
2022-02-06 12:14:51 +0100

    Disable WS server;

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const logger = require('./loggers')();
 const app = require('./app');
 const config = require('./config');
-const initWSServer = require('./ws/ws-server');
+// const initWSServer = require('./ws/ws-server');
 
 const port = config.get('PORT:HTTP');
 app.set('port', port);
@@ -13,10 +13,10 @@ const server = http.createServer(app);
 server.listen(port, () => {
     logger.info('Server is ready on', port);
 
-    initWSServer({
-        port: config.get('PORT:WS'),
-        cookieParser: app.cookieParser,
-        profileLoader: app.profileLoader,
-        profileCreator: app.profileCreator,
-    });
+    // initWSServer({
+    //     port: config.get('PORT:WS'),
+    //     cookieParser: app.cookieParser,
+    //     profileLoader: app.profileLoader,
+    //     profileCreator: app.profileCreator,
+    // });
 });
